b7d200c9091f67dc05c96f9542e15f78f113b507
LC <libertycodeg@gmail.com>
2024-11-06 21:04:39 +0300

    ch thms

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import "./index.css";
+import "@/app/styles/index.css";
 import { App } from './app/App';
 import { BrowserRouter } from "react-router-dom";
 
