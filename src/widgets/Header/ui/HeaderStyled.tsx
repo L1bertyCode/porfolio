bae24b0c6c1a5df61e2588ade8dd8d56d4ea5f90
LC <libertycodeg@gmail.com>
2024-11-12 20:54:09 +0300

    ch w/H

--- src/widgets/Header/ui/HeaderStyled.tsx
+++ src/widgets/Header/ui/HeaderStyled.tsx
@@ -3,13 +3,13 @@ import { routesConfig } from "@/shared/config/routesConfig";
 import { AppLink } from "@/shared/ui/AppLink/AppLink";
 import { AppLinkStyled } from "@/shared/ui/AppLink/AppLinkStyled";
 import { Logo } from "@/shared/ui/Logo/Logo";
-import { useState } from "react";
-import styled from 'styled-components';
+import { ButtonHTMLAttributes, useState } from "react";
+import styled, { css } from 'styled-components';
 
 
 export const HeaderStyled = () => {
   const { toggleTheme } = useTheme();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <StyledHeader>
       <Logo />
@@ -18,7 +18,11 @@ export const HeaderStyled = () => {
           {Object.values(routesConfig).map(route => <AppLinkStyled key={route.path} to={route.path || "/"}>{route.name}</AppLinkStyled>)}
           {Object.values(routesConfig).map(route => <AppLink key={route.path} to={route.path || "/"}>{route.name}</AppLink>)}
         </StyledNav>
-        <StyledBurger />
+        <StyledBurger open={open} onClick={() => {
+          setOpen(open === true ? false : true);
+          console.log("open", open);
+
+        }} />
         <button onClick={toggleTheme}>
           Theme
         </button>
@@ -45,33 +49,74 @@ const StyledNav = styled.nav`
   }
  
 `;
-const StyledBurger = styled.button`
+interface StyledBurgerProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  open?: boolean;
+}
+const StyledBurger = styled.button.attrs<StyledBurgerProps>(({ open }) => ({ open: open }))`
   background: none;
   outline: none;
   border: none;
   padding: 0;
   margin: 0;
-  
-  display: block;
-  margin: var(--indent-s) var(--indent-m);
-  content: "";
-  width: 30px;
-  height: 2px;
-  background: white;
-  position: relative;
 
-  &:before,&:after{
-  position: absolute;
-  content: "";
-  width: 30px;
-  height: 2px;
-  background: white;
-  top: 8px;
-  left: 0;
-  right: 0;
-  }
-  &:after{top:-8px}
-  @media (min-width:767px) {
-    display: none;
-  }
+  ${(open) => {
+    return !open ? css`
+      display: block;
+      margin: var(--indent-s) var(--indent-m);
+      content: "";
+      width: 30px;
+      height: 2px;
+      background: transparent;
+      position: relative;
+
+      &:before{
+      position: absolute;
+      content: "";
+      width: 30px;
+      height: 2px;
+      background: white;
+      left: 0;
+      right: 0;
+      transform: rotate(45deg);
+      }
+    &:after{
+      position: absolute;
+      content: "";
+      width: 30px;
+      height: 2px;
+      background: white;
+  
+      left: 0;
+      right: 0;
+      transform: rotate(-45deg);
+      }
+      @media (min-width:767px) {
+        display: none;
+      }
+    ` : css`
+      display: block;
+      margin: var(--indent-s) var(--indent-m);
+      content: "";
+      width: 30px;
+      height: 2px;
+      background: white;
+      position: relative;
+
+      &:before,&:after{
+      position: absolute;
+      content: "";
+      width: 30px;
+      height: 2px;
+      background: white;
+      top: 8px;
+      left: 0;
+      right: 0;
+      }
+      &:after{top:-8px}
+      @media (min-width:767px) {
+        display: none;
+      }
+    `;
+  }}
+
 `;
